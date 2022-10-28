ae17347aa0a05d3e64a9f90cf07c1b4fe112b515
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2022-10-28 15:56:17 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css'
+import Twitterside from "./Twitterside.js";
+import TwitterFeed from "./TwitterFeed";
+import TwitterWidgets from "./TwitterWidgets";
+function App() {
+  return (
+    
+    <div className="App">
+      <Twitterside/>
+      <TwitterFeed/>
+      <TwitterWidgets/>
+    </div>
+  );
+}
+
+export default App;
+// https://my-twitter-clone-89177.web.app
